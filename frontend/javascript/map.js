778bdcb4b6f18f9d6907404f94813cbe718cfd7e
Matias Korhonen <me@matiaskorhonen.fi>
2022-02-13 14:26:09 +0200

    Draw a choropleth map instead of bubbles

--- frontend/javascript/map.js
+++ frontend/javascript/map.js
@@ -24,17 +24,11 @@ async function initializeMap(svgElement) {
     let data = initialize[1];
 
     // Create a color scale
-    const color = d3
-      .scaleOrdinal()
-      .domain(data.map((d) => d.subregion))
-      .range(d3.schemePaired);
-
-    // Add a scale for bubble size
     const valueExtent = d3.extent(data, (d) => +d.percentage);
-    const size = d3
-      .scaleSqrt()
-      .domain(valueExtent) // What's in the data
-      .range([2, 15]); // Size in pixel
+    const color = d3
+      .scaleSequential()
+      .domain(valueExtent)
+      .interpolator(d3.interpolateLab(d3.hsl(328, 0.3, 0.8), d3.hsl(328, 0.8, 0.55)));
 
     // Draw the map
     const backgroundColor = "#000024";
@@ -42,6 +36,9 @@ async function initializeMap(svgElement) {
     const graticuleStroke = 0.4;
     const mapColor = "#f767b3";
     const graticule = d3.geoGraticule10();
+    // .-. ..- -... -.--
+    // R   U   B    Y
+    const dashArray = "2 2 4 2 2 4 2 2 2 2 4 4 4 2 2 2 2 2 2 4 4 2 2 2 4 2 4 2";
     svg
       .append("path")
       .data([{ type: "Sphere" }])
@@ -59,75 +56,22 @@ async function initializeMap(svgElement) {
       .attr("d", d3.geoPath().projection(projection))
       .style("stroke", graticuleColor)
       .style("stroke-width", graticuleStroke)
+      .style("stroke-dasharray", dashArray)
       .style("opacity", 1);
+
     svg
       .append("g")
       .selectAll("path")
       .data(dataGeo.features)
-      .join("path")
-      .attr("fill", mapColor)
+      .enter()
+      .append("path")
       .attr("d", d3.geoPath().projection(projection))
-      .style("stroke", mapColor)
-      .style("stroke-width", graticuleStroke)
-      .style("opacity", 1);
-
-    // Add circles:
-    svg
-      .selectAll("myCircles")
-      .data(data.sort((a, b) => +b.percentage - +a.percentage).filter((d, i) => i < 1000))
-      .join("circle")
-      .attr("cx", (d) => projection([+d.longitude, +d.latitude])[0])
-      .attr("cy", (d) => projection([+d.longitude, +d.latitude])[1])
-      .attr("r", (d) => size(+d.percentage))
-      .style("fill", (d) => color(d.subregion))
-      .attr("stroke", "none")
-      .attr("stroke-width", 1)
-      .attr("fill-opacity", 0.8);
-
-    // --------------- //
-    // ADD LEGEND //
-    // --------------- //
-
-    // // Add legend: circles
-    // const valuesToShow = [10, 20, 40];
-    // const xCircle = valuesToShow[valuesToShow.length - 1];
-    // const xLabel = 90;
-    // svg
-    //   .selectAll("legend")
-    //   .data(valuesToShow)
-    //   .join("circle")
-    //   .attr("cx", xCircle)
-    //   .attr("cy", (d) => height - size(d))
-    //   .attr("r", (d) => size(d))
-    //   .style("fill", "none")
-    //   .attr("stroke", "white");
-
-    // // Add legend: segments
-    // svg
-    //   .selectAll("legend")
-    //   .data(valuesToShow)
-    //   .join("line")
-    //   .attr("x1", (d) => {
-    //     console.log({ xCircle, d, "size(d)": size(d) });
-    //     return xCircle;
-    //   })
-    //   .attr("x2", xLabel)
-    //   .attr("y1", (d) => height - size(d) * 2)
-    //   .attr("y2", (d) => height - size(d) * 2)
-    //   .attr("stroke", "white")
-    //   .style("stroke-dasharray", "2,2");
-
-    // // Add legend: labels
-    // svg
-    //   .selectAll("legend")
-    //   .data(valuesToShow)
-    //   .join("text")
-    //   .attr("x", xLabel + 2)
-    //   .attr("y", (d) => height - size(d) * 2)
-    //   .text((d) => `${d}%`)
-    //   .style("font-size", 8)
-    //   .style("fill", "white")
-    //   .attr("alignment-baseline", "middle");
+      .attr("fill", (d) => {
+        const stats = data.find((c) => c.alpha3 == d.id);
+        console.log({ stats, ...d });
+        d.percentage = stats?.percentage || 0;
+        return color(d.percentage);
+      });
   });
 }
 
